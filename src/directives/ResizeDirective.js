ef764b3a7ca155120d3f3c2bca774f5e20ab0390
MKhanina <mutterdu@gmail.com>
2022-11-14 23:45:19 +0300

    refactoring ResizeDirective

--- src/directives/ResizeDirective.js
+++ src/directives/ResizeDirective.js
@@ -1,20 +1,25 @@
-let ro = null;
+function inserted (el, binding) {
+  const callback = binding.value;
+  this.ro = new ResizeObserver(([entry]) => {
+    const { contentRect } = entry;
+    const { height, width } = contentRect;
+    
+    callback({
+      height: height,
+      width: width
+    })
+  });
+  this.ro.observe(el);
+}
 
-export default {
-  inserted(el, binding) {
-    const callback = binding.value;
-    ro = new ResizeObserver(([entry]) => {
-      const { contentRect } = entry;
-      const { height, width } = contentRect;
-      
-      callback({
-        height: height,
-        width: width
-      })
-    });
-    ro.observe(el);
-  },
-  unbind(el) {
-    ro.unobserve(el);
-  }
+function unbind(el) {
+  this.ro.unobserve(el);
+}
+
+const ResizeDirective = {
+  ro: null
 };
+ResizeDirective.inserted = inserted.bind(ResizeDirective);
+ResizeDirective.unbind = unbind.bind(ResizeDirective);
+
+export default ResizeDirective;
